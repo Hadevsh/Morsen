869fd7eb4e45ba57eac60b5c0b85182ad690ce97
Wiktor Skrzynecki <skrzynecki.dev@gmail.com>
2025-04-29 19:00:54 +0200

    Add sound generation when click dit and dah buttons

--- js/index.js
+++ js/index.js
@@ -5,3 +5,27 @@ keyMode.addEventListener("click", () => {
     keyModeBin = !keyModeBin;
     console.log(keyModeBin);
 })
+
+const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+function playTone(duration = 100) {
+  const oscillator = audioCtx.createOscillator();
+  oscillator.frequency.setValueAtTime(600, audioCtx.currentTime); // 600Hz tone
+  oscillator.connect(audioCtx.destination);
+  oscillator.start();
+  oscillator.stop(audioCtx.currentTime + duration / 1000);
+}
+
+const ditKey = document.getElementById("dit");
+const dahKey = document.getElementById("dah");
+
+let WPM = 20; // Words per minute (paddle)
+let ditDuration = 1200 / WPM; // 1 unit
+let dahDuration = ditDuration * 3; // 3 units
+
+ditKey.addEventListener("click", () => {
+    playTone(ditDuration);
+})
+
+dahKey.addEventListener("click", () => {
+    playTone(dahDuration);
+})
