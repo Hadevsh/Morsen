524d8e92ba27a07f01f62c87d7953f09319c6956
Wiktor Skrzynecki <skrzynecki.dev@gmail.com>
2025-04-29 22:48:23 +0200

    Add user input display in dits and dahs

--- js/index.js
+++ js/index.js
@@ -8,16 +8,17 @@ keyMode.addEventListener("click", () => {
 
 const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
 function playTone(duration = 100) {
-  const oscillator = audioCtx.createOscillator();
-  oscillator.frequency.setValueAtTime(600, audioCtx.currentTime); // 600Hz tone
-  oscillator.connect(audioCtx.destination);
-  oscillator.start();
-  oscillator.stop(audioCtx.currentTime + duration / 1000);
+    const oscillator = audioCtx.createOscillator();
+    oscillator.frequency.setValueAtTime(600, audioCtx.currentTime); // 600Hz tone
+    oscillator.connect(audioCtx.destination);
+    oscillator.start();
+    oscillator.stop(audioCtx.currentTime + duration / 1000);
 }
 
 const ditKey = document.getElementById("dit");
 const dahKey = document.getElementById("dah");
 const WPM = document.getElementById("wpm"); // Words per minute (paddle)
+const userInput = document.getElementById("input");
 
 let WPMval = WPM.value;
 let ditDuration = 1200 / WPMval; // 1 unit
@@ -25,14 +26,30 @@ let dahDuration = ditDuration * 3; // 3 units
 
 WPM.addEventListener("change", () => {
     WPMval = document.getElementById("wpm").value;
+    ditDuration = 1200 / WPMval; // 1 unit
+    dahDuration = ditDuration * 3; // 3 units
 });
 
+let keyerDown = 0;
+let keyerUp = 0;
+let resetInteval = 1200; // 1 unit between characters (but stay 1200 because to fast)
+
 ditKey.addEventListener("click", () => {
+    keyerUp = performance.now();
+    if (userInput.innerHTML === "Dit ..dit" || (keyerUp-keyerDown) >= resetInteval) { 
+        userInput.innerHTML = ``;
+    }
+    userInput.innerHTML += `•`;
     playTone(ditDuration);
+    keyerDown = performance.now();
 });
 
 dahKey.addEventListener("click", () => {
+    keyerUp = performance.now();
+    if (userInput.innerHTML === "Dit ..dit" || (keyerUp-keyerDown) >= resetInteval) { userInput.innerHTML = `` }
+    userInput.innerHTML += `−`;
     playTone(dahDuration);
+    keyerDown = performance.now();
 });
 
 let oscillator = null;
